d5ea557177832787c9e3b4f809803150a3993df7
ty2green <42952046+ty2green@users.noreply.github.com>
2018-09-04 12:14:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export PYTHONUNBUFFERED=0
-python style.py --style images/aldunate.jpg \
+python style.py --style images/sketch.jpg \
   --checkpoint-dir checkpoints/ \
   --model-dir models/ \
   --test images/violetaparra.jpg \
